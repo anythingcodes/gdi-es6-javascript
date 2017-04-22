9af714d432471821de41a6564cc930d05cd5c09f
Liz Shaw <liz.shaw.medtouch@gmail.com>
2017-04-22 15:03:45 -0400

    Fix editor styling

--- src/js/textarea-transpiler.js
+++ src/js/textarea-transpiler.js
@@ -1,36 +1,42 @@
-function transpile(e, inputEl, outputEl) {
-    var code = (e.keyCode || e.which);
-    if(code !== 37 && code !== 38 && code !== 39 && code !== 40) {
-        try {
-            const transformed = Babel.transform(inputEl.innerText, {
-                presets: ['es2015', 'stage-0']
-            });
-            if (outputEl.classList.contains('error')) {
-                outputEl.classList.remove('error');
-            }
-            outputEl.innerHTML = transformed.code;
-            window.hljs.highlightBlock(outputEl);
-        } catch (ex) {
-            if (!outputEl.classList.contains('error')) {
-                outputEl.classList.add('error');
-            }
-            outputEl.innerText = 'ERROR: ' + ex.message;
+function transpile(inputEl, outputEl) {
+    try {
+        const transformed = Babel.transform(inputEl.innerText, {
+            presets: ['es2015', 'stage-0']
+        });
+        if (outputEl.classList.contains('error')) {
+            outputEl.classList.remove('error');
         }
+        outputEl.innerHTML = transformed.code;
+        if(window.hljs) {
+            window.hljs.highlightBlock(outputEl);
+        }
+    } catch (ex) {
+        if (!outputEl.classList.contains('error')) {
+            outputEl.classList.add('error');
+        }
+        outputEl.innerText = 'ERROR: ' + ex.message;
+    }
+}
+
+function addListenerMulti(el, s, fn) {
+    const events = s.split(' ');
+    for(var i = 0; i < events.length; i++) {
+        el.addEventListener(events[i], fn, false)
     }
 }
 
 function ready() {
     const transpilers = document.getElementsByClassName('transpiler');
-
     [].forEach.call(transpilers, function(transpiler) {
-        var input = transpiler.querySelectorAll('.transpiler__input')[0];
-        var output = transpiler.querySelectorAll('.transpiler__output')[0];
-        input.addEventListener('keydown', function (e) {
-            transpile(e, input, output);
+        const input = transpiler.querySelectorAll('.transpiler__input')[0];
+        const output = transpiler.querySelectorAll('.transpiler__output')[0];
+        addListenerMulti(input, 'keyup keydown', function (e) {
+            const code = (e.keyCode || e.which);
+            if(!code || code !== 37 && code !== 38 && code !== 39 && code !== 40) {
+                transpile(input, output);
+            }
         });
-        input.addEventListener('keyup', function (e) {
-            transpile(e, input, output);
-        });
+        transpile(input, output);
     });
 }
 
